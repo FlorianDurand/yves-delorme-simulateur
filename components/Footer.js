bf87479b9decb585959d87b4a028f85f303f2089
Florian Durand <contact@floriandurand.com>
2020-02-06 12:49:17 +0100

    css pour les nuls

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ import Button from './Styles/Button'
 const Footer = () => (
     <div className={styles.footer}>
         <div className={styles.buttons}>
-            <Button>Taies</Button>
+            <div className={styles.button}><Button>Taies</Button></div>
             <div className={styles.button}><Button>Housse de couette</Button></div>
             <div className={styles.button}><Button>Drap plat</Button></div>
             <div className={styles.button}><Button>Drap housse</Button></div>
