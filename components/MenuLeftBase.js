10b6b46c1eca9977e29a9f59719a1227f014d449
Florian Durand <contact@floriandurand.com>
2020-02-10 18:33:38 +0100

    left menu

---
+++ components/MenuLeftBase.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './MenuLeft.module.css';
+import Button from './Styles/Button';
+
+const MenuLeftBase = props => {
+
+	return (
+		<div className={styles.subSection}>
+			<h2 className={styles.subtitle}>{props.name}</h2>
+			<div className={styles.contents}>
+				{props.contents.map(content=>
+					<button className={styles.button} style={{width: `${props.column}`}}> {content} </button>
+				)} 
+			</div>
+		</div>
+	)
+
+};
+
+
+
+export default MenuLeftBase;
