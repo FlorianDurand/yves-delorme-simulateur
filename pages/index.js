45869a3bd9beb4b75efcfa0219128581ccb4754a
Florian <contact@floriandurand.com>
2020-02-12 14:30:37 +0100

    left menu again

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Header from '../components/Header';
 import Bed from '../components/Bed';
 import Footer from '../components/Footer';
 import Menu from '../components/Menu';
+import MenuLeft from '../components/MenuLeft';
 
 import styles from './index.module.css'
 
