459dd556282d9e1472f0f068d232399f44f3d79e
Marc <marc.ott@hetic.net>
2020-04-13 17:49:05 +0200

    new page is here

---
+++ components/ParureCard.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import styles from './ParureCard.module.scss';
+
+
+const ParureCard = () => (
+
+  <div className={styles.parureCard}>
+    <div className={styles.image}>
+      <div className={styles.icons}>
+        <div className={styles.cart}>
+          <img src="/static/icons/iconAddBasket.svg" alt="cart" />
+        </div>
+        <div className={styles.bin}>
+          <img src="/static/icons/iconBin.svg" alt="bin" />
+        </div>
+      </div>
+    </div>
+    <div className={styles.saveBar}>
+      <p>Parure 1</p>
+      <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M16 29L24.2929 20.7071C24.6834 20.3166 24.6834 19.6834 24.2929 19.2929L16 11" stroke="white" strokeWidth="3" strokeLinecap="round" />
+      </svg>
+    </div>
+  </div>
+
+);
+
+
+export default ParureCard;
---
+++ pages/collection.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import styles from './collection.module.scss';
+import ParureCard from '../components/ParureCard';
+import HeaderCollection from '../components/HeaderCollection';
+
+
+const Collection = () => (
+
+  <div className={styles.collection}>
+    <HeaderCollection />
+    <div className={styles.parures}>
+      <div className={styles.add}>
+        <button type="button">
+          <img src="/static/icons/iconMore.svg" alt="add" />
+        </button>
+        <h2>Nouvelle Parure</h2>
+      </div>
+      <div className={styles.parure}>
+        <ParureCard />
+      </div>
+      <div className={styles.parure}>
+        <ParureCard />
+      </div>
+      <div className={styles.parure}>
+        <ParureCard />
+      </div>
+    </div>
+
+  </div>
+
+);
+
+
+export default Collection;
