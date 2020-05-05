841b2dcbbd9dcdb9c8227580b14374f039e42582
Florian Durand <contact@floriandurand.com>
2020-05-05 14:41:10 +0200

    resize for fit properly to ipad

--- pages/index.jsx
+++ pages/index.jsx
@@ -24,6 +24,13 @@ const SaveIdLog = () => {
 };
 
 const Index = () => {
+  useEffect(() => {
+     window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  })
+
+  });
   const {
     idLog, saveLog, parures, activeParure, initialParureId,
   } = SaveIdLog();
